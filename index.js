1373cf372a3a853aed761734cea4959740384d30
MD Rajib <83833885+blackweb71@users.noreply.github.com>
2021-11-18 12:22:39 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+x = document.querySelector("title");
+a = document.querySelector(".but1");
+b = document.querySelector(".but2");
+c = document.querySelector(".but3");
+d = document.querySelector(".but4");
+e = document.querySelector(".but5");
+f = document.querySelector(".but6");
+g = document.querySelector(".but7");
+h = document.querySelector(".but8");
+i = document.querySelector(".but9");
+
+x.innerHTML="Villain";
+a.innerHTML="Rawkto Rawhoshyo 2021 Bengali Movie 720p WEB-DL 800MB";
+b.innerHTML="SOS Kolkata 2020 Bengali Movie 720p ZEE5 WEB-DL 750MB";
+c.innerHTML="Abhimaan 2016 Bengali Movie 720p WEB-DL 1GB";
+d.innerHTML="Ebar Shabor 2015 Bengali Movie 720p HEVC HDRip 550MB";
+e.innerHTML="Dujone";
+f.innerHTML="F.I.R";
+g.innerHTML="Villain";
+f
+h.innerHTML="Ajob Premer Golpo";
+i.innerHTML="Tumi Ashbe Bole";
